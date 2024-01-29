62e15cb77c0f7bf17b67a6469c08508c00ecd6cf
Tars Verschelde <tars.verschelde@kulueven.be>
2024-01-29 10:19:20 +0100

    fixed documentation location

--- docs/make.jl
+++ docs/make.jl
@@ -2,23 +2,21 @@ using Documenter
 
 pages=[
     "Home" => "index.md",
-    #=
     "Third Party Tools" => Any[
         "CorRES" => joinpath("third_party_tools", "CorRES.md"),
         "AIDRES" => joinpath("third_party_tools", "AIDRES.md"),
         "ChaProEV" => joinpath("third_party_tools", "ChaProEV.md")
-    ]
-    =#
+    ],
     "Examples" => Any[
         "Stepstone exercise" => joinpath("examples", "stepstone_exercise.md"),
-        "Workflow 20230815" => joinpath("examples", "workflow_20230815.md")
+        #"Workflow 20230815" => joinpath("examples", "workflow_20230815.md")
     ]
 ]
 
 makedocs(
-    sitename="component-tools",
+    sitename="ines-data-tools",
     format=Documenter.HTML(prettyurls=get(ENV, "CI", nothing) == "true"),
     pages=pages,
 )
 
-deploydocs(repo="github.com/spine-tools/component-tools.git", versions=["stable" => "v^", "v#.#"])
+deploydocs(repo="github.com/energy-modelling-workbench/ines-data-tools.git", versions=["stable" => "v^", "v#.#"])
