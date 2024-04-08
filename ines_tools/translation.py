ac287bbb03b01dbcb0490ad5198917e86a3d491a
Tars Verschelde <tars.verschelde@kulueven.be>
2024-04-08 13:50:02 +0200

    use spine database

--- ines_tools/translation.py
+++ ines_tools/translation.py
@@ -1,6 +1,7 @@
 import sys
 import json
 from importlib.machinery import SourceFileLoader
+import spinedb_api as api
 
 #path = dirname(@__DIR__)
 #tool = "pypsa"#"spineopt"#
@@ -21,18 +22,17 @@ map.map_preprocess(iodb)
 # Load input data
 # assume that input follows the generic energy format template
 print("Load input data")
-with open(input) as f:
-	inputdata = json.load(f)
+if input.split(".")[-1] == 'json':
+	with open(input) as f:
+		inputdata = json.load(f)
+else:
+	with api.DatabaseMapping(input) as source_db:
+		inputdata = api.export_data(source_db,parse_value=api.parameter_value.load_db_value)
 
-entitynames = {}
 entitydict = {}
 for entity in inputdata["entities"]:
-	if len(entity[2])>1:
-		entityrelation = entity[2]
-	else:
-		entityrelation = entity[1]
-	entitynames[entity[1]] = entityrelation
-	entitydict[entity[1]] = entity
+	entityrelation = ''.join(entity[1])
+	entitydict[entityrelation] = entity
 
 parameterdict = {}
 for parameterdefinition in inputdata["parameter_definitions"]:
@@ -45,7 +45,7 @@ for parameterdefinition in inputdata["parameter_definitions"]:
 
 entityparameterdict = {}
 for parametervalue in inputdata["parameter_values"]:
-	entityname = entitynames[parametervalue[1]]
+	entityname = ''.join(parametervalue[1])
 	if entityname not in entityparameterdict:
 		entityparameterdict[entityname] = {}
 	entityparameterdict[entityname][parametervalue[2]] = parametervalue[3]
@@ -62,11 +62,16 @@ for (entityname,entity) in entitydict.items():
 # map the input data to the output data
 print("Map input data to output data")
 for (entityname, entityparameters) in entityparameterdict.items():
-	entities = [entityname]
+	# an entity (or relation between entities) and its parameters
+	entities = [entitydict[entityname][1]]
 	parameters = [entityparameters]
-	for entityrelation in entitydict[entityname][2]:
-		entities.append(entityrelation)
-		parameters.append(entityparameterdict[entityrelation])
+	if type(entitydict[entityname][1]) != str:
+		# list or tuple
+  		# in other words, if the entity describes a relation
+		# we add the related entities as they may be needed in the translation
+		for entityrelation in entitydict[entityname][1]:
+			entities.append(entityrelation)
+			parameters.append(entityparameterdict[entityrelation])
 	getattr(map,"map_"+entitydict[entityname][0])(iodb,entities,parameters)
 
 # postprocess
@@ -75,6 +80,16 @@ map.map_postprocess(iodb)
 
 # write data to output
 print("Writing data")
-with open(output, "w") as f:
-	json.dump(iodb, f, indent=4)
-	
+if output.split(".")[-1] == 'json':
+	with open(output, 'w') as f:
+		json.dump(iodb, f, indent=4)
+else:
+	with api.DatabaseMapping(output) as target_db:
+		target_db.purge_items('parameter_value')
+		target_db.purge_items('entity')
+		target_db.purge_items('alternative')
+		target_db.refresh_session()
+		target_db.commit_session("Purge items")
+		api.import_data(target_db,**iodb)
+		target_db.refresh_session()
+		target_db.commit_session("Import data")
